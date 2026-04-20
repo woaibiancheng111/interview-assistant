046df6a8495225d817da41ac5d6d30d8f378be7f
MH <m19109088555@163.com>
2026-04-20 15:47:36 +0800

    style: 修复eslint配置中的拼写错误

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
-    "out/**",
+    "out/**",ghh
     "build/**",
     "next-env.d.ts",
   ]),
